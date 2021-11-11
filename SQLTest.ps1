954c16e868fbbfae50521e9ba6e8a97c2d15c516
DykemaBill <git@dykema.org>
2021-11-11 16:56:07 -0600

    Added long running query

--- SQLTest.ps1
+++ SQLTest.ps1
@@ -5,6 +5,7 @@
 #  Date        : 2021-11-08
 # ====================================================================================
 #  2021-11-08 - Dykema, Bill - initial version
+#  2021-11-11 - Dykema, Bill - added long running query
 # ====================================================================================
  
 # Setup variables
@@ -81,7 +82,7 @@ function SQLTables() {
     # Database tables
     $DBTablesQuery = GetSqlQuery($SQLDBTables)
  
-    # View talbes
+    # View tables
     $DBTablesQuery | Format-Table
  
 }
@@ -100,32 +101,43 @@ function SQLTest() {
  
     # Start
     Write-Host "Running SQL query from $env:COMPUTERNAME..."
- 
-    # Test query
-    $SQLQuery = "SELECT * from $SQLTable"
- 
-    # Time stamp
-    $RunDateTime = "$(Get-Date -UFormat '%Y-%m-%d_%H%M')"
+    $RunDateTime = "$(Get-Date -UFormat '%Y-%m-%d_%T')"
     Write-Host "$RunDateTime - $SQLServerInstance server, $SQLDB database, $SQLTable table query starting"
     "$RunDateTime - $SQLServerInstance server, $SQLDB database, $SQLTable table query starting" | Out-File -FilePath $LogFile -Append
  
-    # Run query
-    $TableQuery = GetSqlQuery($SQLQuery)
+    # Get rows in table
+    $SQLRows = "SELECT (SELECT COUNT(*) FROM $SQLTable) AS 'Rows'"
+    $RowsQuery = GetSqlQuery($SQLRows)
+    $RunDateTime = "$(Get-Date -UFormat '%Y-%m-%d_%T')"
+    Write-Host "$RunDateTime - $($RowsQuery.Rows) rows returned"
+    "$RunDateTime - $($RowsQuery.Rows) rows returned" | Out-File -FilePath $LogFile -Append
  
-    # Number of rows returned
-    $RunDateTime = "$(Get-Date -UFormat '%Y-%m-%d_%H%M')"
-    $RowsReturned = ($TableQuery | Format-Table).Count
-    Write-Host "$RunDateTime - $RowsReturned rows returned"
-    "$RunDateTime - $RowsReturned rows returned" | Out-File -FilePath $LogFile -Append
+    # Get column names
+    $SQLColumns = "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'$SQLTable'"
+    $ColumnsQuery = GetSqlQuery($SQLColumns)
+    $ColumnNames = $ColumnsQuery.COLUMN_NAME
+    $RunDateTime = "$(Get-Date -UFormat '%Y-%m-%d_%T')"
+    Write-Host "$RunDateTime - $($ColumnNames.count) column names gathered"
+    "$RunDateTime - $($ColumnNames.count) column names gathered" | Out-File -FilePath $LogFile -Append
+    #$ColumnNames | Format-Table
  
-    # View data
-    #Write-Host "Table query result..."
-    #$TableQuery | Format-Table
+    # Run query using each column to sort
+    Foreach ($ColumnName in $ColumnNames) {
+        $RunDateTime = "$(Get-Date -UFormat '%Y-%m-%d_%T')"
+        Write-Host "$RunDateTime - Sorting with $ColumnName..."
+        $SQLSorted = "SELECT * FROM $SQLTable ORDER BY [$ColumnName] ASC"
+        $ColumnsQuery = GetSqlQuery($SQLSorted)
+    }
+ 
+    # Completed
+    $RunDateTime = "$(Get-Date -UFormat '%Y-%m-%d_%T')"
+    Write-Host "$RunDateTime - Completed running all sorts"
+    "$RunDateTime - Completed running all sorts" | Out-File -FilePath $LogFile -Append
  
 }
  
 function SQLOptions {
-    Write-Output "`nPlease use the following format to run a query against a table:"
+    Write-Output "`nPlease use the following format to run a long running query against a table:"
     Write-Output "`t$ScriptName [SQLServer\SQLInstance] [SQLDatabase] [SQLTable]"
     Write-Output "`nPlease use the following format to list tables in a database:"
     Write-Output "`t$ScriptName [SQLServer\SQLInstance] [SQLDatabase]`n"
