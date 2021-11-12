c42593f6a1646b3da8af2a96043f6b9e39693b5a
DykemaBill <git@dykema.org>
2021-11-12 08:37:15 -0600

    Custom schema handling

--- SQLTest.ps1
+++ SQLTest.ps1
@@ -7,12 +7,12 @@
 #  2021-11-08 - Dykema, Bill - initial version
 #  2021-11-11 - Dykema, Bill - added long running query
 # ====================================================================================
- 
+
 # Setup variables
 $ScriptName = $MyInvocation.MyCommand
 $RunDir = "$(Get-Location)"
 $LogFile = "$($RunDir)\SQLTest_$($env:COMPUTERNAME)"
- 
+
 # SQL connection
 function GetSqlConnection
 {
@@ -30,28 +30,28 @@ function GetSqlConnection
             return $null
         }
 }
- 
+
 # SQL query function
 function GetSqlQuery($SQLQueryPassed)
 {
     $SQLConnection = GetSqlConnection
- 
+
     $SQLCmd = New-Object System.Data.SqlClient.SqlCommand
- 
+
     $SQLCmd.CommandText = $SQLQueryPassed
     $SQLCmd.Connection = $SQLConnection
- 
+
     $SQLReader = $SQLCmd.ExecuteReader()
- 
+
     $SQLTableData = New-Object System.Data.DataTable
     $SQLTableData.Load($SQLReader)
     $SQLConnection.Close()
- 
+
     return $SQLTableData
 }
- 
+
 function SQLTables() {
- 
+
     Param
     (
          [Parameter(Mandatory=$true, Position=0)]
@@ -59,10 +59,10 @@ function SQLTables() {
          [Parameter(Mandatory=$true, Position=1)]
          [string] $SQLDB
     )
- 
+
     # Start
     Write-Host "Running SQL query from $env:COMPUTERNAME..."
- 
+
     # Query to list database tables
     $SQLDBTables =  "DECLARE @QueryString NVARCHAR(MAX) ;" +
                     "SELECT @QueryString = COALESCE(@QueryString + ' UNION ALL ','')
@@ -78,17 +78,17 @@ function SQLTables() {
                         AND sOBJ.is_ms_shipped = 0x0
                     ORDER BY SCHEMA_NAME(sOBJ.schema_id), sOBJ.name ;
                     EXEC sp_executesql @QueryString"
- 
+
     # Database tables
     $DBTablesQuery = GetSqlQuery($SQLDBTables)
- 
+
     # View tables
     $DBTablesQuery | Format-Table
- 
+
 }
- 
+
 function SQLTest() {
- 
+
     Param
     (
          [Parameter(Mandatory=$true, Position=0)]
@@ -98,20 +98,28 @@ function SQLTest() {
          [Parameter(Mandatory=$true, Position=1)]
          [string] $SQLTable
     )
- 
+
+    # Break table into schema and table
+    if ($SQLTable -like '*.*') {
+        $SQLSchema = ($SQLTable -split "\.")[0]
+        $SQLTable = ($SQLTable -split "\.")[1]
+    } else {
+        $SQLSchema = "dbo"
+    }
+
     # Start
     Write-Host "Running SQL query from $env:COMPUTERNAME..."
     $RunDateTime = "$(Get-Date -UFormat '%Y-%m-%d_%T')"
-    Write-Host "$RunDateTime - $SQLServerInstance server, $SQLDB database, $SQLTable table query starting"
-    "$RunDateTime - $SQLServerInstance server, $SQLDB database, $SQLTable table query starting" | Out-File -FilePath $LogFile -Append
- 
+    Write-Host "$RunDateTime - $SQLServerInstance server, $SQLDB database, $SQLSchema.$SQLTable table query starting"
+    "$RunDateTime - $SQLServerInstance server, $SQLDB database, $SQLSchema.$SQLTable table query starting" | Out-File -FilePath $LogFile -Append
+
     # Get rows in table
-    $SQLRows = "SELECT (SELECT COUNT(*) FROM $SQLTable) AS 'Rows'"
+    $SQLRows = "SELECT (SELECT COUNT(*) FROM $SQLSchema.$SQLTable) AS 'Rows'"
     $RowsQuery = GetSqlQuery($SQLRows)
     $RunDateTime = "$(Get-Date -UFormat '%Y-%m-%d_%T')"
     Write-Host "$RunDateTime - $($RowsQuery.Rows) rows returned"
     "$RunDateTime - $($RowsQuery.Rows) rows returned" | Out-File -FilePath $LogFile -Append
- 
+
     # Get column names
     $SQLColumns = "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'$SQLTable'"
     $ColumnsQuery = GetSqlQuery($SQLColumns)
@@ -120,29 +128,29 @@ function SQLTest() {
     Write-Host "$RunDateTime - $($ColumnNames.count) column names gathered"
     "$RunDateTime - $($ColumnNames.count) column names gathered" | Out-File -FilePath $LogFile -Append
     #$ColumnNames | Format-Table
- 
+
     # Run query using each column to sort
     Foreach ($ColumnName in $ColumnNames) {
         $RunDateTime = "$(Get-Date -UFormat '%Y-%m-%d_%T')"
         Write-Host "$RunDateTime - Sorting with $ColumnName..."
-        $SQLSorted = "SELECT * FROM $SQLTable ORDER BY [$ColumnName] ASC"
+        $SQLSorted = "SELECT * FROM $SQLSchema.$SQLTable ORDER BY [$ColumnName] ASC"
         $ColumnsQuery = GetSqlQuery($SQLSorted)
     }
- 
+
     # Completed
     $RunDateTime = "$(Get-Date -UFormat '%Y-%m-%d_%T')"
     Write-Host "$RunDateTime - Completed running all sorts"
     "$RunDateTime - Completed running all sorts" | Out-File -FilePath $LogFile -Append
- 
+
 }
- 
+
 function SQLOptions {
     Write-Output "`nPlease use the following format to run a long running query against a table:"
     Write-Output "`t$ScriptName [SQLServer\SQLInstance] [SQLDatabase] [SQLTable]"
     Write-Output "`nPlease use the following format to list tables in a database:"
     Write-Output "`t$ScriptName [SQLServer\SQLInstance] [SQLDatabase]`n"
 }
- 
+
 # Read arguments provided
 if ($args.length -lt 2) { # No option provided
     SQLOptions
